c4c0a90e830be63598a05bad025bb3486d6bdc65
ausprouty <bob.prouty@powertochange.org.au>
2020-11-17 15:38:46 +1100

    Attempting Language Menu

---
+++ js/Language.js
@@ -0,0 +1,17 @@
+function languageMenu(id) {
+  document.getElementById(id).classList.toggle("show");
+
+ 
+    
+    /*
+    https://www.w3schools.com/howto/howto_js_dropdown.asp
+    
+    <div id="myDropdown" class="dropdown-content">
+      <a href="#">(Arabic) عربى</a>
+      <a href="#">English</a>
+      <a href="#">(Farsi) فارسی</a>
+      <a href="#">(Urdu) اردو</a>
+    </div>
+    */
+ 
+  }
